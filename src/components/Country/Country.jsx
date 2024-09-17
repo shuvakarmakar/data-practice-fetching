8b11ec0fd027c561fb0224ee788efe702c18375b
shuvakarmakar <shuva15-7953@diu.edu.bd>
2024-09-17 23:34:09 +0600

    Header Added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,14 @@
-const Country = ({country}) => {
-    console.log(country);
+import './Country.css'
 
-    const {name} = country //destructuring
+const Country = ({country}) => {
+    // console.log(country);
+
+    const {name, flags} = country //destructuring
     return (
-        <div>
+        <div className="country">
             <h3>Name: {name.common}</h3>
+            <img src={flags.png} alt="" />
+            <h4>This is Each countries Flag</h4>
         </div>
     );
 };
