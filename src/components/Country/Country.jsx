1534321b328159ff9bd011306cf8963c793f7944
shuvakarmakar <shuva15-7953@diu.edu.bd>
2024-09-18 18:03:39 +0600

    Fetch Area , population and Capital of Each Country

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,16 @@
 import './Country.css'
 
 const Country = ({ country }) => {
-    console.log(country);
+    // console.log(country);
 
-    const { name, flags, capital } = country //destructuring
+    const { name, flags, capital, population, area } = country //destructuring
     return (
         <div className="country">
             <h3>Name: {name.common}</h3>
             <img src={flags.png} alt="" />
             <h3>Capital of This Country : {capital}</h3>
+            <h4>Total Area of this country: {area}</h4>
+            <h4>Population of this country: {population}</h4>
             <h4>This is Each countries Flag</h4>
         </div>
     );
